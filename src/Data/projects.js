f3db227faba71fad815466f6dede4c4e87df4e93
Douglas Wiggins <douglas.wiggins@snhu.edu>
2025-11-13 17:34:17 -0500

    Import images as modules for proper bundling

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -1,24 +1,28 @@
 // Sample data for projects
+import planetAppImage from "../assets/3d planet app screenshot.png";
+import colorixImage from "../assets/colorix screenshot.png";
+import superpollImage from "../assets/Screenshot 2025-11-13 at 4.18.18 PM.png";
+
 export const projects = [
   {
     id: 1,
     title: "3D Planet App",
     description: "Displays planets and their info with stunning visuals.",
-    image: "./assets/3d planet app screenshot.png",
+    image: planetAppImage,
     link: "#",
   },
   {
     id: 2,
     title: "Colorix",
     description: "Mix colors and display hex values with live previews.",
-    image: "./assets/colorix screenshot.png",
+    image: colorixImage,
     link: "https://github.com/basslove28/Colorix",
   },
   {
     id: 3,
     title: "Superpoll",
     description: "A polling application for creating and managing polls",
-    image: "./assets/Screenshot 2025-11-13 at 4.18.18 PM.png",
+    image: superpollImage,
     link: "https://github.com/basslove28/superpoll_v1",
   },
 ];
